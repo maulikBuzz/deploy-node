829f1ecef41fb44cc9ee898ffd670a593db8211f
maulik-buzz <maulik.buzz@gmail.com>
2024-08-23 15:17:32 +0530

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
+require('dotenv').config()
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("App works!!");
