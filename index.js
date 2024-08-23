c4742c79011d987127cad8790e8d8bb01c7672ac
maulik-buzz <maulik.buzz@gmail.com>
2024-08-23 15:44:12 +0530

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 require('dotenv').config()
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, '')));
+
+// Redirect the root URL to the index.html file
 app.get("/", (req, res) => {
-  res.send("App works!!");
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.listen(port, (err) => {
   console.log(`running server on from port::::::: http://localhost:${port}`, err);
